ae229008bce4d40d1ad600783a62b41c0ca75e6d
cramonDE <cramonDE@github.com>
2022-11-13 22:25:42 +0800

    feat: add currify func

---
+++ js/closure/curry.js
@@ -0,0 +1,47 @@
+// https://zh.javascript.info/currying-partials
+
+
+// example1 简单应用
+function curry(f) { // curry(f) 执行柯里化转换
+  return function(a) {
+    return function(b) {
+      return f(a, b);
+    };
+  };
+}
+
+// 用法
+function sum(a, b) {
+  return a + b;
+}
+
+let curriedSum = curry(sum);
+
+alert( curriedSum(1)(2) ); // 3
+
+// example2 实际应用，轻松生成偏函数，收敛共同逻辑
+
+function curry(func) {
+
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+
+}
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum2 = curry(sum);
+
+alert( curriedSum(1, 2, 3) ); // 6，仍然可以被正常调用
+alert( curriedSum(1)(2,3) ); // 6，对第一个参数的柯里化
+alert( curriedSum(1)(2)(3) ); // 6，全柯里化
+
